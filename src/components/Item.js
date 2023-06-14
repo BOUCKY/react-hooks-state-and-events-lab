90ce018f35a38e3bc01db201eb737071449dc904
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-06-14 15:58:44 -0400

    Complete Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  // Set inCart initial value to false (there is not something in the cart)
+  const [inCart, setCart] = useState(false)
+
+  // When onClick is triggered, toggle inCart state between true and false
+  const handleCartToggle = () => {
+    setCart(!inCart)
+  }
+
   return (
-    <li className="">
+    // if inCart is true, assign 'in-cart' class name. If it's false, don't assign a class name
+    <li className={inCart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  // Set initial value to "All" so all the items on the list show
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  // When onChange is triggered, update selectedCategory based on the selected value
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value)
+  }
+
+  // Filter through items array. If category = "All", assign entire items array to filteredItems, otherwise, use filter method to create a new array containing only the items that match the value of selectedCategory.
+  const filteredItems = items.filter(
+    (item) => selectedCategory === "All" || item.category === selectedCategory
+  );
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +28,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {/* Map through 'filteredItems' instead of 'items' */}
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +38,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
